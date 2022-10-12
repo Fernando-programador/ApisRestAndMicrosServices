5b5d9b1bc06f4ca11b3c467cbc00763ef6cf98f5
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2022-10-12 18:20:06 -0300

    instalado config-app

---
+++ MicrosservicosExemplo/config-app/src/main/java/com/example/configapp/ConfigAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.configapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@enablecon
+public class ConfigAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigAppApplication.class, args);
+	}
+
+}
---
+++ MicrosservicosExemplo/config-app/src/test/java/com/example/configapp/ConfigAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
