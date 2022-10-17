8d5e5144e0afed41214707a6b817962e8b80bfeb
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2022-10-17 12:42:00 -0300

    teste de valores

--- TesteProjetos/projeto-tdd/src/main/java/com/example/projetotdd/model/Comissao.java
+++ TesteProjetos/projeto-tdd/src/main/java/com/example/projetotdd/model/Comissao.java
@@ -4,6 +4,6 @@ public class Comissao {
     
 
     public Double calcular(Double valorVenda){
-        return valorVenda <= 1000.0 ? valorVenda * 0.1 : valorVenda * 0.15 ;
+        return valorVenda >= 1000.0 ? valorVenda * 0.1 : valorVenda * 0.15 ;
     }
 }
--- TesteProjetos/projeto-tdd/src/test/java/com/example/projetotdd/model/ComissaoTest.java
+++ TesteProjetos/projeto-tdd/src/test/java/com/example/projetotdd/model/ComissaoTest.java
@@ -2,18 +2,50 @@ package com.example.projetotdd.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 public class ComissaoTest {
+
+    @TestConfiguration
+static class ComissaoConfig{
+
+    @Bean
+    public Comissao comissao() {
+        return new Comissao();
+    }
+}
+
+@Autowired
+Comissao comissao;
+
     
     @Test
-    public void deve_calculcar_100_reias_comissao_venda_1000(){
+    public void deve_calculcar_100_reias_comissao_venda_1000_com_10_por_cento(){
 
         //arrange = preparação
-        Comissao comissao = new Comissao();
+         //Comissao comissao = new Comissao();
         Double valorVenda = 1000.00;
-        Double valorComissao = 0.0;
+        Double valorComissao = 100.0;
+
+        //act = ação
+        Double valorCalculado = comissao.calcular(valorVenda);
+
+
+        //assert = confirmação
+        Assertions.assertEquals(valorComissao, valorCalculado);
+    }
+
+    @Test
+    public void deve_calculcar_300_reias_comissao_venda_2000_com_10(){
+
+        //arrange = preparação
+        //Comissao comissao = new Comissao();
+        Double valorVenda = 2000.00;
+        Double valorComissao = 200.0;
 
         //act = ação
         Double valorCalculado = comissao.calcular(valorVenda);
